ab7f451bafb5d1f5776aebb24bba08289fcef19d
tjehret <tjehret@gmx.de>
2021-09-09 08:45:19 +0200

    Add wish-to-be-files
    add my "wish-folder".

---
+++ ScriptSplitterPlugin/whatIwant/client/client.js
@@ -0,0 +1,6 @@
+import { registerBpmnJSPlugin, registerClientExtension } from 'camunda-modeler-plugin-helpers';
+
+import ScriptSplitterPlugin from '../ScriptSplitterPlugin';
+
+
+registerClientExtension(ScriptSplitterPlugin);
---
+++ ScriptSplitterPlugin/whatIwant/menu/menu.js
@@ -0,0 +1,26 @@
+var example = require("../ScriptSplitterPlugin");
+
+/**
+* add menu entry
+*/
+module.exports = function(electronApp, menuState) {
+  return [{
+    label: 'Execute ScriptSplitter',
+    enabled: function() {
+      // only enabled for BPMN diagrams
+      return menuState.bpmn;
+    },
+    action: function(){
+      test(); 
+    }
+  }];
+};
+
+
+/**
+* create my shapes here
+* e.g. this creates a template for the moment.
+*/
+function test(){
+  console.log("hurra")
+}
