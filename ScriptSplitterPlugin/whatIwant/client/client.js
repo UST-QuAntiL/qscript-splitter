2dfe364da06d7e25524651145daad57fc8ab3da3
Tim-Julian Ehret <tjehret@gmx.de>
2021-10-10 18:20:36 +0200

    Delete ScriptSplitterPlugin directory

--- ScriptSplitterPlugin/whatIwant/client/client.js
+++
@@ -1,6 +0,0 @@
-import { registerBpmnJSPlugin, registerClientExtension } from 'camunda-modeler-plugin-helpers';
-
-import ScriptSplitterPlugin from '../ScriptSplitterPlugin';
-
-
-registerClientExtension(ScriptSplitterPlugin);
--- ScriptSplitterPlugin/whatIwant/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  script: './client/client.js',
-  //script: './client/client.bundle.js',
-  menu: './menu/menu.js', // test
-  name: 'ScriptSplitterPlugin'
-};
