2211034571491c4813f5422a762f71d6475a046d
Daniel Vietz <vietzd@gmail.com>
2022-03-06 14:55:49 +0100

    Improve method extraction to allow multiple splits within one code block

--- app/script_splitting/labeler.py
+++ app/script_splitting/labeler.py
@@ -22,10 +22,60 @@ from app import app
 from app.script_splitting.Labels import Labels
 import numpy as np
 import logging
+import random
+import string
 
 
-def is_hybrid(splitting_labels):
-    return Labels.CLASSICAL in splitting_labels and Labels.QUANTUM in splitting_labels
+def get_all_external_variables_of_code_block(code_block):
+    initialized_variables = []
+    external_variables = []
+    for line in code_block:
+        created_variables, referenced_variables = get_all_variables_of_line(line)
+        # check if referenced variables of line have been initialized before
+        # add to external_variables if not
+        for referenced_variable in referenced_variables:
+            if referenced_variable not in initialized_variables:
+                external_variables.append(referenced_variable)
+        initialized_variables.extend(referenced_variables)
+
+    return external_variables
+
+
+def get_all_variables_of_line(line):
+    created_variables = []
+    referenced_variables = []
+
+    if line.type == "assignment":
+        if line.target.type == 'tuple':
+            for variable in line.target.value:
+                created_variables.append(variable.value)
+        else:
+            created_variables.append(line.target.value)
+
+    # TODO: Get used variables
+
+    return created_variables, referenced_variables
+
+
+def extract_method(code_block):
+    logging.info("Extract code block to separate function...")
+
+    # create method name
+    letters = string.ascii_lowercase
+    method_name = ''.join(random.choice(letters) for i in range(10))
+
+    # create parameters list
+    variables = get_all_external_variables_of_code_block(code_block)
+    parameters = ",".join(variables)
+
+    # create new def node
+    create_str = "def extracted_method_" + method_name + "(" + parameters + "):"
+
+    # add lines to def node
+    for line in code_block:
+        create_str += "\n    " + str(line.dumps())
+
+    return RedBaron(create_str)
 
 
 def split_code_block(root_baron, method_baron, splitting_labels):
@@ -34,24 +84,21 @@ def split_code_block(root_baron, method_baron, splitting_labels):
         if method_baron.value[i].type != "endl":
             logging.debug("[%i] %s [%s]" % (i, method_baron.value[i], splitting_labels[i]))
 
-    first_block = []
-    first_block_label = splitting_labels[0]
-    while splitting_labels[0] == first_block_label:
-        to_add = method_baron.value[0]
-        first_block.append(to_add)
-        splitting_labels.pop(0)
-
-    logging.info("Extract first code block to separate function...")
-    # create new def node
-    create_str = "def new_block():"
-    for line in first_block:
-        create_str += "\n    " + str(line.value)
-    logging.debug('Extracted method:\n%s' % create_str)
-    def_node = RedBaron(create_str)
-
-    # TODO add to root document
-    # TODO set parameters correctly
-    # TODO run in a loop/recursively (while method is_hybrid)
+    to_extract = []
+    result_definitions = []
+    for i in range(len(splitting_labels)):
+        if len(to_extract) == 0:
+            logging.debug("Start new block")
+        if method_baron.value[i].type == "endl":
+            continue
+        logging.debug('Add %s to current block' % method_baron.value[i].dumps())
+        to_extract.append(method_baron.value[i])
+        # for very last element or when next elements label is different
+        if i == len(splitting_labels)-1 or splitting_labels[i+1] != splitting_labels[i]:
+            new_method = extract_method(to_extract)
+            logging.debug("Created method: %s" % repr(new_method.dumps()))
+            result_definitions.append(new_method)
+            to_extract = []
 
 
 def split_local_function(root_baron, method_baron, white_list, black_list, label_map, quantum_objects):
