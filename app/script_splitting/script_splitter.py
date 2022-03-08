9a3be3ee7226ad0d6dbe2891053987179abb9d61
Daniel Vietz <vietzd@gmail.com>
2022-03-08 23:16:40 +0100

    Fix bug during splitting: reimplement logic for grouping labeled code lines into code blocks

--- app/script_splitting/script_splitter.py
+++ app/script_splitting/script_splitter.py
@@ -24,36 +24,49 @@ import logging
 
 def split_script(script, splitting_labels):
     for i in range(len(script)):
-        print("%s: %s" % (splitting_labels[i], repr(script[i].dumps())))
+        logging.debug("%s: %s" % (splitting_labels[i], repr(script[i].dumps())))
 
-    to_extract = []
-    result_definitions = []
-    counter = 0
+    all_code_blocks = []
+    code_block = []
+    current_label = None
     for i in range(len(splitting_labels)):
-        if splitting_labels[i] == Labels.IMPORTS or script[i].type == "endl":
+        label = splitting_labels[i]
+        line = script[i]
+        # skip imports
+        if label == Labels.IMPORTS:
             continue
-        if len(to_extract) == 0:
-            logging.debug("Start new block")
-            counter += 1
-        logging.debug('Add %s to current block' % repr(script[i].dumps()))
-        to_extract.append(script[i])
-        # for very last element or when next elements label is different
-        if is_end_of_code_block(i, splitting_labels):
-            new_method = extract_method(to_extract, counter)
-            print(new_method.dumps())
-            logging.debug("Created method: %s" % repr(new_method.dumps()))
-            result_definitions.append(new_method)
-            to_extract = []
+        # add empty lines to code block, too
+        if label == Labels.NO_CODE:
+            code_block.append(line)
+            continue
+        # tag label of current block
+        if current_label is None:
+            current_label = label
+        # start new code block if label changes
+        if label != current_label:
+            current_label = label
+            all_code_blocks.append(code_block[:])
+            code_block = []
+        # add line to code block
+        code_block.append(line)
+    # add last code block
+    all_code_blocks.append(code_block[:])
+
+    counter = 0
+    for code_block in all_code_blocks:
+        counter += 1
+        return_variables = compute_return_variables(code_block, all_code_blocks)
+        parameters = compute_parameters(code_block)
+        extracted_method = extract_method(code_block, counter, parameters, return_variables)
+        print(extracted_method.dumps())
 
 
-def is_end_of_code_block(i, splitting_labels):
-    return i == len(splitting_labels) - 1 or \
-           (splitting_labels[i + 1] != splitting_labels[i]
-            and not splitting_labels[i] == Labels.NO_CODE
-            and not splitting_labels[i + 1] == Labels.NO_CODE)
+def compute_return_variables(code_block, all_code_blocks):
+    # TODO
+    return ["a", "b", "c"]
 
 
-def get_all_external_variables_of_code_block(code_block):
+def compute_parameters(code_block):
     initialized_variables = []
     external_variables = []
     for line in code_block:
@@ -85,44 +98,21 @@ def get_all_variables_of_line(line):
     return created_variables, referenced_variables
 
 
-def extract_method(code_block, counter):
+def extract_method(code_block, counter, parameters, needed_variables):
     logging.info("Extract code block to separate function...")
 
     # create method name
     method_name = 'function_' + str(counter)
 
-    # create parameters list
-    variables = get_all_external_variables_of_code_block(code_block)
-    parameters = ",".join(variables)
-
     # create new def node
-    create_str = "def " + method_name + "(" + parameters + "):"
+    create_str = "def " + method_name + "(" + ",".join(parameters) + "):"
 
     # add lines to def node
     for line in code_block:
         create_str += "\n    " + str(line.dumps())
 
+    # add return statement
+    if len(needed_variables) > 0:
+        create_str += "\n    " + "return " + ", ".join(needed_variables)
+
     return RedBaron(create_str)
-
-
-def split_code_block(root_baron, method_baron, splitting_labels):
-    logging.info('Break up code block into several parts based on final labels:')
-    for i in range(len(method_baron.value)):
-        if method_baron.value[i].type != "endl":
-            logging.debug("[%i] %s [%s]" % (i, method_baron.value[i], splitting_labels[i]))
-
-    to_extract = []
-    result_definitions = []
-    for i in range(len(splitting_labels)):
-        if len(to_extract) == 0:
-            logging.debug("Start new block")
-        if method_baron.value[i].type == "endl":
-            continue
-        logging.debug('Add %s to current block' % method_baron.value[i].dumps())
-        to_extract.append(method_baron.value[i])
-        # for very last element or when next elements label is different
-        if i == len(splitting_labels)-1 or splitting_labels[i+1] != splitting_labels[i]:
-            new_method = extract_method(to_extract)
-            logging.debug("Created method: %s" % repr(new_method.dumps()))
-            result_definitions.append(new_method)
-            to_extract = []
